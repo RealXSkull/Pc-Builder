40214e32180e1f687173a94ab1357d9274c132b2
danish-sheikh600 <danish.shk09@gmail.com>
2022-11-30 17:27:06 +0500

    sidebar
    sidebar creation started

--- lib/MainMenu.dart
+++ lib/MainMenu.dart
@@ -1,9 +1,10 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, file_names, prefer_final_fields, unnecessary_new, use_key_in_widget_constructors, avoid_print, non_constant_identifier_names, sized_box_for_whitespace, must_call_super, unnecessary_import
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, file_names, prefer_final_fields, unnecessary_new, use_key_in_widget_constructors, avoid_print, non_constant_identifier_names, sized_box_for_whitespace, must_call_super, unnecessary_import, depend_on_referenced_packages
 
 // import 'package:fyp/LoginScreen.dart';
 // import 'package:fluttertoast/fluttertoast.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:fyp/NavBar.dart';
 
 class MainMenu extends StatefulWidget {
   const MainMenu({super.key});
